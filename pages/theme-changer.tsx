b73cf70bf29ffea35792cc40fbfe5a8b0b0fd0b1
alan <alanarostegui@hotmail.com>
2022-08-01 23:54:43 -0500

    feat: add js cookie

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -9,12 +9,15 @@ import {
   Radio,
 } from "@mui/material";
 import { Layout } from "../components/layouts";
+import Cookies from 'js-cookie';
 
 const ThemeChangerPage = () => {
   const [currentTheme, setCurrentTheme] = useState("light");
 
   const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
     setCurrentTheme(event.target.value);
+
+    Cookies.set('theme', event.target.value);
   };
 
   return (
